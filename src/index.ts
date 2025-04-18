b6f1d158cd38b34a2c5f459b96011c6f5f4f79cd
joealdersonstrachan <joealdersonstrachan@strachan.email>
2025-04-18 18:07:31 +0100

    feat: update index

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { app } from '@app/api';
-import { db } from '@datr.tech/leith-common-mongodb-connector';
 import { logger } from '@datr.tech/leith-common-logger';
+import { db } from '@datr.tech/leith-common-mongodb-connector';
 import 'dotenv/config';
 
 const { API_NAME, API_PORT } = process.env;
